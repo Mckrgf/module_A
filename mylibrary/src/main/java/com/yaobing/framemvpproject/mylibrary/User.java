a64cf9c9002c3a0bfa414be7ff35d9656eaced0a
姚冰321 <1124507487@qq.com>
2022-10-07 16:26:29 +0800

    子模块带上dcendent插件（看看能不能解决jitpack的git error）

---
+++ mylibrary/src/main/java/com/yaobing/framemvpproject/mylibrary/User.java
@@ -0,0 +1,12 @@
+package com.yaobing.framemvpproject.mylibrary;
+
+public class User {
+    String name = "";
+    int age = 1;
+    public User(String name,Integer age) {
+
+    }
+    public User(String name) {
+
+    }
+}
---
+++ mylibrary/src/main/java/com/yaobing/framemvpproject/mylibrary/UserDao.kt
@@ -0,0 +1,9 @@
+package com.yaobing.framemvpproject.mylibrary
+
+class UserDao {
+    var name = ""
+    var age = 1
+
+    constructor(name: String?, age: Int?) {}
+    constructor(name: String?) {}
+}
