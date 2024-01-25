4999bf3cbf8f4afa776c49c8fa815b7278750a3e
890063 <890063@leapmotor.com>
2024-01-25 15:02:26 +0800

    ADD:glide apng

---
+++ mylibrary/src/main/java/com/yaobing/framemvpproject/mylibrary/BaseGlideModule.kt
@@ -0,0 +1,7 @@
+package com.yaobing.framemvpproject.mylibrary
+
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.module.AppGlideModule
+@GlideModule
+class BaseGlideModule:AppGlideModule() {
+}
--- mylibrary/src/main/java/com/yaobing/framemvpproject/mylibrary/TestActivity.kt
+++ mylibrary/src/main/java/com/yaobing/framemvpproject/mylibrary/TestActivity.kt
@@ -1,11 +1,23 @@
 package com.yaobing.framemvpproject.mylibrary
 
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.PixelFormat
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.text.TextUtils
 import android.util.Log
 import android.view.View
 import androidx.core.widget.addTextChangedListener
+import com.bumptech.glide.Glide
+import com.bumptech.glide.RequestBuilder
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
+import com.github.penfeizhou.animation.apng.APNGDrawable
+import com.github.penfeizhou.animation.gif.GifDrawable
 import com.yaobing.framemvpproject.mylibrary.activity.IntentRouter
 import com.yaobing.framemvpproject.mylibrary.databinding.ActivityTestBinding
 import com.yaobing.framemvpproject.mylibrary.function.JavaBestSingleton
@@ -13,6 +25,9 @@ import com.yaobing.framemvpproject.mylibrary.function.SingletonKotlin
 import com.yaobing.module_apt.*
 import com.yaobing.module_middleware.Utils.*
 import com.yaobing.module_middleware.activity.BaseActivity
+import java.io.ByteArrayInputStream
+import java.io.ByteArrayOutputStream
+import java.io.InputStream
 import java.lang.reflect.Field
 import java.util.*
 
@@ -87,8 +102,48 @@ class TestActivity : BaseActivity() {
                 ToastUtils.show(this,e.toString())
             }
         }
+        Glide.with(context)
+//            .asGif()
+            .load("file:///android_asset/test5.png")
+            .listener(object : RequestListener<Drawable?> {
+            override fun onLoadFailed(e: GlideException?, model: Any, target: Target<Drawable?>, isFirstResource: Boolean): Boolean {
+                return false
+            }
+
+            override fun onResourceReady(resource: Drawable?, model: Any, target: Target<Drawable?>, dataSource: DataSource, isFirstResource: Boolean): Boolean {
+                if (resource is APNGDrawable) {
+                    (resource as APNGDrawable).setLoopLimit(10)
+                }
+                return false
+            }
+        })
+            .into(binding.ivDfds)
+
+    }
+    public fun bitmapInputStream(bm: Bitmap, quality:Int) : InputStream {
+        val baos = ByteArrayOutputStream()
+        bm.compress(Bitmap.CompressFormat.PNG, quality, baos)
+        return ByteArrayInputStream(baos.toByteArray())
     }
 
+
+    fun drawableToBitmap(drawable: Drawable): Bitmap? {
+        val bitmap = Bitmap
+            .createBitmap(
+                drawable.intrinsicWidth,
+                drawable.intrinsicHeight,
+                if (drawable.opacity != PixelFormat.OPAQUE) Bitmap.Config.ARGB_8888 else Bitmap.Config.RGB_565
+            )
+        val canvas = Canvas(bitmap)
+
+// canvas.setBitmap(bitmap);
+        drawable.setBounds(
+            0, 0, drawable.intrinsicWidth,
+            drawable.intrinsicHeight
+        )
+        drawable.draw(canvas)
+        return bitmap
+    }
     //kotlin匿名函数
     val stringLengthFunc: (String) -> String = { input ->
         "输入的字符串$input 的长度为" + input.length
