5a9559065cc518081d322bfde824f6c3e960597e
890063 <890063@leapmotor.com>
2024-07-18 19:30:26 +0800

    能够根据网络请求更新数据了 -next： 研究下生命周期和点击时间

--- mylibrary/src/main/java/com/yaobing/framemvpproject/mylibrary/AlphaWidgetProvider.kt
+++ mylibrary/src/main/java/com/yaobing/framemvpproject/mylibrary/AlphaWidgetProvider.kt
@@ -7,6 +7,15 @@ import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.widget.RemoteViews
+import com.yaobing.framemvpproject.mylibrary.network.APIService
+import com.yaobing.module_middleware.network.Api
+import io.reactivex.Scheduler
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
 
 class AlphaWidgetProvider : AppWidgetProvider() {
     var appWidgetManager: AppWidgetManager? = null
@@ -52,6 +61,21 @@ class AlphaWidgetProvider : AppWidgetProvider() {
         val views = RemoteViews(context.packageName, R.layout.alpha_widget_layout)
         views.setTextViewText(R.id.tv_name, "update了")
         appWidgetManager.updateAppWidget(appWidgetIds, views)
+
+        val retrofit = Retrofit.Builder().baseUrl("https://api.github.com/").addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+            .addConverterFactory(GsonConverterFactory.create())
+            .build();
+
+        var disposable = retrofit.create(APIService::class.java)
+            .listReposRx("MCKRGF")
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe {
+                Log.d("zxcv", "获取到数据了 +$it")
+                val views = RemoteViews(context.packageName, R.layout.alpha_widget_layout)
+                views.setTextViewText(R.id.tv_name, "data: $it")
+                appWidgetManager.updateAppWidget(appWidgetIds, views)
+            }
     }
 
     override fun onEnabled(context: Context) {
