e2ee9c631b12f78fbf464b63e71e36a748e824ca
890063 <890063@leapmotor.com>
2024-09-26 16:30:45 +0800

    筛选框的ui逻辑;空布局

--- mylibrary/src/main/java/com/yaobing/framemvpproject/mylibrary/activity/DoubleRecyclerViewActivity.kt
+++ mylibrary/src/main/java/com/yaobing/framemvpproject/mylibrary/activity/DoubleRecyclerViewActivity.kt
@@ -1,6 +1,9 @@
 package com.yaobing.framemvpproject.mylibrary.activity
 
+import android.content.Context
+import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
+import android.location.LocationManager
 import android.os.Bundle
 import android.view.View
 import android.view.ViewGroup
@@ -41,12 +44,34 @@ class DoubleRecyclerViewActivity : AppCompatActivity() {
         findViewById<View>(R.id.viewMask)
     }
 
+    private val demoAdapterA by lazy {
+        DemoAdapter()
+    }
+    private val demoAdapterB  by lazy {
+        DemoAdapter()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_double_recyclerview)
         StatusBarUtil.setAppStatusBar(this)
+        initLocation()
         initShopData()
         initSortData()
+    }
+
+    private fun initLocation() {
+    }
+
+    /**
+     * 手机是否开启位置服务，如果没有开启那么所有app将不能使用定位功能
+     */
+    fun isLocServiceEnable(context: Context): Boolean {
+        val locationManager: LocationManager =
+            context.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        val gps: Boolean = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
+        val network: Boolean = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
+        return gps || network
     }
 
     private fun initSortData() {
@@ -54,6 +79,7 @@ class DoubleRecyclerViewActivity : AppCompatActivity() {
         val tvType = findViewById<TextView>(R.id.tvType)
 
         val types = mutableListOf<String>()
+        types.add("全部类型")
         types.add("美食")
         types.add("住宿")
         types.add("体验")
@@ -66,7 +92,7 @@ class DoubleRecyclerViewActivity : AppCompatActivity() {
 
         popupMenu.isFocusable = false
         popupMenu.isOutsideTouchable = true
-        popupMenu.animationStyle = R.style.pop_option_animation
+//        popupMenu.animationStyle = R.style.pop_option_animation
 
         popupMenu.setBackgroundDrawable(
             ColorDrawable(
@@ -79,48 +105,74 @@ class DoubleRecyclerViewActivity : AppCompatActivity() {
         popupMenu.setOnDismissListener {
             vMask.visibility = View.GONE
         }
+        val sortAdapter = PopWindowOptionAdapter(sorts)
+        val typeAdapter = PopWindowOptionAdapter(types)
+
         tvSort.setOnClickListener {
             if (popupMenu.isShowing) {
-
                 popupMenu.dismiss()
             } else {
-                vMask.visibility = View.VISIBLE
                 val rv = popupMenu.contentView.findViewById<RecyclerView>(R.id.rv)
                 rv.layoutManager = LinearLayoutManager(this)
-                val sortAdapter = PopWindowOptionAdapter(sorts)
                 rv.adapter = sortAdapter
                 sortAdapter.setNewData(sorts)
+                sortAdapter.emptyView = View.inflate(this@DoubleRecyclerViewActivity,R.layout.no_shop_view,null)
                 popupMenu.showAsDropDown(tvSort)
                 popupMenu.isFocusable = true
+                sortAdapter.setOnItemClickListener { adapter, view, position ->
+                    if (position == 0) {
+                        tvSort.setTextColor(Color.parseColor("#333333"))
+                    }else {
+                        tvSort.setTextColor(resources.getColor(R.color.tabLineA))
+                    }
+                    popupMenu.dismiss()
+                    tvSort.text = sorts[position]
+                    sortAdapter.currentType = sorts[position]
+                    //顺便展示下空数据
+                    demoAdapterA.setNewData(null)
 
+                }
+                vMask.visibility = View.VISIBLE
             }
         }
         tvType.setOnClickListener {
             if (popupMenu.isShowing) {
                 popupMenu.dismiss()
             } else {
+
                 vMask.visibility = View.VISIBLE
                 val rv = popupMenu.contentView.findViewById<RecyclerView>(R.id.rv)
                 rv.layoutManager = LinearLayoutManager(this)
-                val typeAdapter = PopWindowOptionAdapter(types)
                 rv.adapter = typeAdapter
                 typeAdapter.setNewData(types)
                 popupMenu.showAsDropDown(tvSort)
                 popupMenu.isFocusable = true
-
+                typeAdapter.setOnItemClickListener { adapter, view, position ->
+                    if (position == 0) {
+                        tvType.setTextColor(Color.parseColor("#333333"))
+                    }else {
+                        tvType.setTextColor(resources.getColor(R.color.tabLineA))
+                    }
+                    popupMenu.dismiss()
+                    tvType.text = types[position]
+                    typeAdapter.currentType = types[position]
+                    //顺便展示下空数据
+                    demoAdapterB.setNewData(null)
+                }
             }
         }
     }
     private fun initShopData() {
-        val demoAdapterA = DemoAdapter()
-        val demoAdapterB = DemoAdapter()
+        demoAdapterA.emptyView = View.inflate(this@DoubleRecyclerViewActivity,R.layout.no_shop_view,null)
+        demoAdapterB.emptyView = View.inflate(this@DoubleRecyclerViewActivity,R.layout.no_shop_view,null)
+
 
         rvA.adapter = demoAdapterA
         rvB.adapter = demoAdapterB
         rvA.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
         rvB.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
         val data = mutableListOf<RepoData>()
-        for (i in 0 until 10) {
+        for (i in 0 until 3) {
             val repoData = RepoData(i)
             repoData.full_name = "店铺名字： +$i"
             repoData.i = Random.nextInt(3,5)
--- mylibrary/src/main/java/com/yaobing/framemvpproject/mylibrary/adapter/PopWindowOptionAdapter.kt
+++ mylibrary/src/main/java/com/yaobing/framemvpproject/mylibrary/adapter/PopWindowOptionAdapter.kt
@@ -7,7 +7,7 @@ import com.yaobing.framemvpproject.mylibrary.R
 class PopWindowOptionAdapter(carType: List<String>?) : BaseQuickAdapter<String, BaseViewHolder>(R.layout.item_option_popwindow) {
     var currentType = carType?.get(0)
     override fun convert(helper: BaseViewHolder, item: String?) {
-        helper.setTextColor(R.id.tv, if (item== currentType) mContext.getColor(R.color.tabLine) else  mContext.getColor(R.color.color_333333))
+        helper.setTextColor(R.id.tv, if (item== currentType) mContext.getColor(R.color.tabLineA) else  mContext.getColor(R.color.color_333333))
         helper.setText(R.id.tv, item ?: "")
     }
 }
