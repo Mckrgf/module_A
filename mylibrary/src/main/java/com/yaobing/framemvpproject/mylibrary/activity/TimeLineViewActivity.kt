fffd17a0f01aae1f31aa433a9e1e3412bd752567
890063 <890063@leapmotor.com>
2024-10-08 17:59:24 +0800

    打卡页-title增加交互效果 next-返回数据插入年份数据

--- mylibrary/src/main/java/com/yaobing/framemvpproject/mylibrary/activity/TimeLineViewActivity.kt
+++ mylibrary/src/main/java/com/yaobing/framemvpproject/mylibrary/activity/TimeLineViewActivity.kt
@@ -1,17 +1,23 @@
 package com.yaobing.framemvpproject.mylibrary.activity
 
+import android.content.res.Resources
 import android.os.Bundle
+import android.util.Log
+import android.util.TypedValue
 import android.view.View
 import android.widget.ImageView
+import android.widget.RelativeLayout
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.OnScrollListener
 import com.yaobing.framemvpproject.mylibrary.R
 import com.yaobing.framemvpproject.mylibrary.adapter.TimeLineAdapter
 import com.yaobing.framemvpproject.mylibrary.data.RepoData
 import com.yaobing.module_apt.Router
+import com.yaobing.module_middleware.Utils.AnimationUtil
+import com.yaobing.module_middleware.Utils.UiTools
 import kotlin.random.Random
-import com.yaobing.module_middleware.Utils.StatusBarUtil
 
 @Router(value = "timeLineRecyclerviewactivity")
 class TimeLineViewActivity : AppCompatActivity() {
@@ -26,19 +32,58 @@ class TimeLineViewActivity : AppCompatActivity() {
         TimeLineAdapter()
     }
     override fun onCreate(savedInstanceState: Bundle?) {
+//        supportRequestWindowFeature(Window.FEATURE_NO_TITLE)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_time_line_recyclerview)
-        StatusBarUtil.setAppStatusBar(this)
+        UiTools.setStatusBarTransparent(this)
+        initListener()
         initShopData()
-    }
+        val header = findViewById<RelativeLayout>(R.id.ll_mine_header)
 
+    }
+    fun dp2px(dp: Float): Float =
+        TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, Resources.getSystem().displayMetrics)
+
+    private fun initListener() {
+        var scrollY = 0
+        var hasShownTitle = false
+        rvTimeLine.addOnScrollListener(object : OnScrollListener() {
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                super.onScrolled(recyclerView, dx, dy)
+                var a = recyclerView.canScrollVertically(-1)
+                scrollY += dy
+                if (recyclerView.canScrollVertically(-1) && scrollY >= dp2px(18f)) {
+                    if (!hasShownTitle) {
+                        hasShownTitle = true
+                        Log.d("zxcv","----1dddd")
+                        fadeAttentionView(true)
+                    }
+                } else {
+                    if (hasShownTitle) {
+                        Log.d("zxcv","----1qqqq")
+                        hasShownTitle = false
+                        fadeAttentionView(false)
+                    }
+                }
+            }
+        })
+    }
+    fun fadeAttentionView(toVisible: Boolean) {
+        val mAnimationUtil = AnimationUtil()
+        val view = findViewById<View>(R.id.v_bg)
+        if (toVisible) {
+            mAnimationUtil.fadeInToVisible(view)
+        } else {
+            mAnimationUtil.fadeOutToInvisible(view)
+        }
+    }
     private fun rotationArrow(img : ImageView, rotate:Float) {
         img.animate().setDuration(0).rotation(rotate)
     }
 
     private fun initShopData() {
         timeLineAdapter.emptyView = View.inflate(this@TimeLineViewActivity,R.layout.no_shop_view,null)
-
+        timeLineAdapter.setHeaderView(View.inflate(this@TimeLineViewActivity,R.layout.rv_header,null))
 
         rvTimeLine.adapter = timeLineAdapter
         rvTimeLine.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
