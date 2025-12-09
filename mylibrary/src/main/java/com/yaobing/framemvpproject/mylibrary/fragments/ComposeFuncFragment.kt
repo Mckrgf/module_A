9338a713cee66d95947daacd8accf66fdefe96dd
姚冰 <890063@leapmotor.com>
2025-12-09 17:38:01 +0800

    compose结合网络请求page3+items

--- mylibrary/src/main/java/com/yaobing/framemvpproject/mylibrary/fragments/ComposeFuncFragment.kt
+++ mylibrary/src/main/java/com/yaobing/framemvpproject/mylibrary/fragments/ComposeFuncFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.annotation.ColorRes
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -14,6 +13,7 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
@@ -26,28 +26,27 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.ComposeView
-import androidx.compose.ui.platform.ViewCompositionStrategy
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.setViewTreeLifecycleOwner
-import androidx.lifecycle.setViewTreeViewModelStoreOwner
-import androidx.savedstate.setViewTreeSavedStateRegistryOwner
 import com.yaobing.framemvpproject.mylibrary.R
 import com.yaobing.framemvpproject.mylibrary.composeView.MessageCard
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.foundation.rememberScrollState
+import androidx.paging.compose.LazyPagingItems
+import androidx.paging.compose.collectAsLazyPagingItems
+import com.yaobing.framemvpproject.mylibrary.data.RepoData
+import com.yaobing.framemvpproject.mylibrary.data.Repository
+import androidx.paging.LoadState
 
 
 open class ComposeFuncFragment : Fragment() {
 
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
-
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, saved: Bundle?) = inflater.inflate(R.layout.fragment_compose, container, false)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, saved: Bundle?) = inflater.inflate(R.layout.fragment_compose, container, false)!!
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         val composeView = view.findViewById<ComposeView>(R.id.composeView)
@@ -55,13 +54,13 @@ open class ComposeFuncFragment : Fragment() {
         composeView.setViewTreeLifecycleOwner(viewLifecycleOwner)
 
         composeView.setContent {
-            setComposeView()
+            ComposeView()
         }
     }
 
-    @Preview
+    @Preview(widthDp = 360, heightDp = 640)
     @Composable
-    open fun setComposeView() {
+    open fun ComposeView() {
         var count by remember { mutableIntStateOf(0) }
         Box(
             modifier = Modifier
@@ -70,9 +69,10 @@ open class ComposeFuncFragment : Fragment() {
         ) {
             Column(
                 modifier = Modifier
-                    .clip(RoundedCornerShape(10))
                     .fillMaxSize()
-                    .background(color = colorResource(id = R.color.white)),
+                    .clip(RoundedCornerShape(10))
+                    .background(color = colorResource(id = R.color.white))
+                    .verticalScroll(rememberScrollState()), // 添加的代码：实现垂直滚动
                 horizontalAlignment = Alignment.CenterHorizontally,
                 verticalArrangement = Arrangement.Center
 
@@ -90,9 +90,13 @@ open class ComposeFuncFragment : Fragment() {
                         verticalAlignment = Alignment.CenterVertically
                     ) {
                         Text(text = "count 0: $count", modifier = Modifier.padding(15.dp))
-                        VerticalDivider(thickness = 1.dp, color = colorResource(id = R.color.color_dddddd), modifier = Modifier.fillMaxHeight().padding(5.dp))
+                        VerticalDivider(thickness = 1.dp, color = colorResource(id = R.color.color_dddddd), modifier = Modifier
+                            .fillMaxHeight()
+                            .padding(5.dp))
                         Text(text = "count 1: $count", modifier = Modifier.padding(15.dp))   // 顺手把 typo 也修了
-                        VerticalDivider(thickness = 1.dp, color = colorResource(id = R.color.color_dddddd), modifier = Modifier.fillMaxHeight().padding(5.dp))
+                        VerticalDivider(thickness = 1.dp, color = colorResource(id = R.color.color_dddddd), modifier = Modifier
+                            .fillMaxHeight()
+                            .padding(5.dp))
                         Text(text = "count 2: $count", modifier = Modifier.padding(15.dp))
                     }
                 }
@@ -104,6 +108,33 @@ open class ComposeFuncFragment : Fragment() {
                     modifier = Modifier.padding(16.dp)
                 ) {
                     Text(text = "Increment")
+                }
+                val pagingItems: LazyPagingItems<RepoData> = Repository.getPagingData().collectAsLazyPagingItems()
+
+                LazyColumn(
+                    modifier = Modifier.height(250.dp)
+                ) {
+                    // TODO: 无法展示动态数据，很奇怪。先展示写死的数据好了 
+                    items(4) { repo ->
+                        repo?.let {
+                            MessageCard(content = "fewfw:" + repo)  // 显示 RepoData 的 name
+                        }
+                    }
+
+                    // 处理加载状态
+                    pagingItems.apply {
+                        when {
+                            loadState.refresh is LoadState.Loading -> {
+                                item { Text("Loading...") }
+                            }
+                            loadState.append is LoadState.Loading -> {
+                                item { Text("Loading more...") }
+                            }
+                            loadState.refresh is LoadState.Error -> {
+                                item { Text("Error: ${(loadState.refresh as LoadState.Error).error.message}") }
+                            }
+                        }
+                    }
                 }
             }
         }
